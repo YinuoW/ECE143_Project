125cfb318dd121082d38102d87feca4bdcba80d1
Yinuo Wang <yiw010@ucsd.edu>
2019-11-04 21:02:14 -0800

    data reload

--- merge.py
+++ merge.py
@@ -8,6 +8,6 @@ all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #print(all_filenames)
 
 #combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
 #export to csv
-combined_csv.to_csv( "2018.csv", index=False)
+combined_csv.to_csv("2018.csv", index=False)
